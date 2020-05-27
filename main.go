5732ed3e2722fd0a31549202295cb35f3bc25465
Elijah W. Gagne <elijah.w.gagne@dartmouth.edu>
2020-05-27 15:34:09 -0400

    Fixed Confluence code bocks

--- main.go
+++ main.go
@@ -37,9 +37,36 @@ func main() {
 	html := string(output)
 	converter := md.NewConverter("", true, nil)
 	converter.Use(plugin.GitHubFlavored())
+	converter.Use(plugin.ConfluenceCodeBlock())
 	markdown, err := converter.ConvertString(html)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(markdown)
 }
+
+// <p>Here is a three line code block</p><ac:structured-macro ac:name="code" ac:schema-version="1" ac:macro-id="17182db8-062e-4400-a53f-04cd1adda8ac"><ac:parameter ac:name="language">bash</ac:parameter><ac:plain-text-body><![CDATA[echo 'hi'
+// cat file.txt | grep begin
+// mv abc xyz]]></ac:plain-text-body></ac:structured-macro><p />
+
+// <p>Here is a three line code block</p>
+// <ac:structured-macro ac:name="code" ac:schema-version="1" ac:macro-id="17182db8-062e-4400-a53f-04cd1adda8ac">
+// 	<ac:parameter ac:name="language">bash</ac:parameter>
+// 	<ac:plain-text-body>
+// 	<![CDATA[echo 'hi'
+// cat file.txt | grep begin
+// mv abc xyz]]>
+// </ac:plain-text-body>
+// </ac:structured-macro>
+// <p />
+
+// Find:
+// <ac:structured-macro ac:name="code" ac:schema-version="1" ac:macro-id="17182db8-062e-4400-a53f-04cd1adda8ac"><ac:parameter ac:name="language">bash</ac:parameter><ac:plain-text-body><![CDATA[
+// Replace: ```bash
+
+// Find:
+// ]]></ac:plain-text-body></ac:structured-macro>
+// Replace: ```
+
+// html := strings.ReplaceAll(string(output), "]]></ac:plain-text-body></ac:structured-macro>", "\n```\n")
+// 	html = strings.ReplaceAll(html, "<ac:structured-macro ac:name=\"code\" ac:schema-version=\"1\" ac:macro-id=\"17182db8-062e-4400-a53f-04cd1adda8ac\"><ac:parameter ac:name=\"language\">bash</ac:parameter><ac:plain-text-body><![CDATA[", "\n```\n")
